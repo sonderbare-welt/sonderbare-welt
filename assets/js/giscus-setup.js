e065372318982696d288a56205d5871c4c564129
sonderbare-welt <ms.s@hotmail.de>
2025-08-30 08:22:03 +0000

    Deploying to gh-pages from @ sonderbare-welt/sonderbare-welt.github.io@d25b81d58dc42590136123396d0d5464c1e128e3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "sonderbare-welt/sonderbare-welt",
+    "data-repo": "sonderbare-welt/sonderbare-welt.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
